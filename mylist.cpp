176801423c24e1e22dc97c14713efce30bbb7a49
Darren Bowers <darren.bowers@g.austincc.edu>
2021-11-10 17:01:51 -0600

    used and modified selectionsort method provided in example for void sort to sort the array if unsorted at time of call

--- mylist.cpp
+++ mylist.cpp
@@ -126,5 +126,16 @@ bool MyList::getSorted(){
 }
 
 void MyList::sort(){
-    
+    if(sorted = false);
+        int i, j, minindex; 
+        for (i = 0; i < length-1; i++) { 
+            minindex = i; 
+            for (j = i+1; j < length; j++) {
+                if (array[j] < array[minindex]) {
+                    minindex = j; 
+                }
+            }
+            swap(&array[minindex], &array[i]); 
+            sorted = true;
+    } 
 }
